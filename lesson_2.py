c675ce7f86daed6414832b8fd9c6c489f4986d7b
Yana-crypto702 <yanaorlova_1@icloud.com>
2024-08-06 17:47:26 +0400

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,6 @@
+number_of_completed_homework='12'
+nuberOfHoursSpent='1.5'
+course_name='Python'
+timeForOneTask='0,125'
+print("Курс:",course_name,',',"всего задач:",number_of_completed_homework, ',',"затрачено часов:",nuberOfHoursSpent, ',', "среднее время выполнения",timeForOneTask,"часа.")
+
