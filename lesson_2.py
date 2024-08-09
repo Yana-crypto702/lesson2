f363f1ee6fe722f1e01e6c57ac6fd45e8fd9d763
Yana-crypto702 <yanaorlova_1@icloud.com>
2024-08-09 15:49:12 +0400

    Update lesson_2.py

--- lesson_2.py
+++ lesson_2.py
@@ -1,6 +1,6 @@
-number_of_completed_homework='12'
-nuberOfHoursSpent='1.5'
+number_of_completed_homework=12
+nuberOfHoursSpent=1.5
 course_name='Python'
-timeForOneTask='0,125'
+timeForOneTask=nuberOfHoursSpent/number_of_completed_homework
 print("Курс:",course_name,',',"всего задач:",number_of_completed_homework, ',',"затрачено часов:",nuberOfHoursSpent, ',', "среднее время выполнения",timeForOneTask,"часа.")
 
